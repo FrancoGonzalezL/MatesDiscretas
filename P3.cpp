ea88cc18a4c61310736f882d57a46edb5978b255
AquariusCL <martin16bravo@gmail.com>
2023-04-05 11:37:06 -0400

    P3 lista, faltan los markdown

---
+++ P3.cpp
@@ -0,0 +1,25 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int F(int n, vector<int> array){
+    if (n == 3){
+        return max(array[1], array[0]+array[2]);
+    }
+    else{
+        return max(F(n - 1, array), F(n -2, array) + array[n-1]);
+    }
+}
+
+int main(){
+    ios_base::sync_with_stdio(0); cin.tie(0);
+    int n;
+    cin >> n;
+    vector<int> valores(n);
+    for (int i = 0; i < n; i++){
+        int num;
+        cin >> num;
+        valores[i] = num;
+    }
+    cout << F(n, valores) << endl;
+    return 0;
+}
