892be1eaeb6d2a063d6865ca672232b2c943521d
FrancoGonzalezL <frango.gonzalez.l@ug.uchile.cl>
2023-04-08 22:12:17 -0400

    borre lo que no servia

--- P3.cpp
+++ P3.cpp
@@ -2,19 +2,24 @@
 using namespace std;
 
 vector<int> F(int n, int array[]){
+
     if(n==1){
         vector<int> posiciones;
         posiciones.push_back(0);
         return posiciones;}
+
     else if (n==2){
         vector<int> posiciones;
+
         if (array[0]>=array[1]){
             posiciones.push_back(0);
             return posiciones;}
+
         else{
             posiciones.push_back(1);
             return posiciones;}
             }
+
     else{
         vector<int> pos1 = F(n-1,array);
         vector<int> pos2 = F(n-2,array);
@@ -22,18 +27,23 @@ vector<int> F(int n, int array[]){
         for(auto itr = pos1.begin(); itr != pos1.end(); itr++){suma1+=array[*itr];}
         for(auto itr = pos2.begin(); itr != pos2.end(); itr++){suma2+=array[*itr];}
         suma2 += suma3;
+
         if (suma1>=suma2){
+
             if(suma1>=suma3){
             return pos1;}
+
             else{
             vector<int> pos3;
             pos3.push_back(n-1);
             return pos3;}
         }
         else{
+
             if(suma2>=suma3){
             pos2.push_back(n-1);
             return pos2;}
+
             else{
             vector<int> pos3;
             pos3.push_back(n-1);
@@ -41,7 +51,6 @@ vector<int> F(int n, int array[]){
         }
     }
 }
-
 
 int main(){
     int n;cin >> n;int valores[n];
@@ -56,36 +65,4 @@ int main(){
     }
     cout<<endl<< suma <<endl;
     return 0;
-}
-
-
-//no funciona con negativos
-vector<int> F1(int n, int array[]){
-    if (n==2){
-        vector<int> posiciones;
-        if (array[0]>=array[1]){posiciones.push_back(0);}
-        else{posiciones.push_back(1);}
-
-        return posiciones;}
-    else if (n == 3){
-        vector<int> posiciones;
-        if (array[0]+array[2]>=array[1]){
-            posiciones.push_back(0);posiciones.push_back(2);
-            return posiciones;}
-        else{
-            posiciones.push_back(1);
-            return posiciones;}
-        }
-    else{
-        vector<int> pos1 = F(n-1,array);
-        vector<int> pos2 = F(n-2,array);
-        int suma1 = 0;
-        int suma2 = 0;
-        for(auto itr = pos1.begin(); itr != pos1.end(); itr++){suma1+=*itr;}
-        for(auto itr = pos2.begin(); itr != pos2.end(); itr++){suma2+=*itr;}
-        if(suma2+array[n-1]>=suma1){
-            pos2.push_back(n-1);
-            return pos2;}
-        else{return pos1;}
-        }
 }
