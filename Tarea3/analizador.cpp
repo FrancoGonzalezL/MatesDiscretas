3a6a60d834ce5562c07a68731b22c99090a142d6
FrancoGonzalezL <frango.gonzalez.l@ug.uchile.cl>
2023-06-09 23:04:40 -0400

    casi listo el if

--- Tarea3/analizador.cpp
+++ Tarea3/analizador.cpp
@@ -6,8 +6,6 @@ using namespace std;
 //61 == "="
 vector<vector<int>> CFG;
 vector<string> Bloques;
-vector<string> texto;
-set<int> variables_definidas;
 
 bool is_alpha(int a){
     return (96<a && a<123);
@@ -87,6 +85,18 @@ bool es_while(string line){
     }
     return s[s.size()-1]==58;
 }
+bool exis_else(string name, int i, int ident_){
+    ifstream ar(name);
+    string line;
+    int c = 0;
+    while(getline(ar,line)){
+        if(c<=i){c++;continue;}//avanzamos a la linea que leimos
+        if(contar_ident(line)==ident_){
+            if(es_if(line))return false;       
+            if(es_else(line))return true;}
+    }
+    return false;
+}
 int main(){
     //abrir el archivo
     string nombre; cin>>nombre;
@@ -96,27 +106,84 @@ int main(){
         return 1;}
 
     string line;
-    while(getline(inputFile,line)){
-        int ident = 0;  
-        string bloque;
+    string bloque;
+    int ident = 0;  
+    int i = 0;
+    int j = 0;
 
+    vector<vector<int>> nodos;
+    vector<int> nodo;
+    stack<int> nodo_while;
+    stack<tuple<int,int,bool>> nodo_if;
+
+    while(getline(inputFile,line)){
         if(ident>contar_ident(line)){
-            Bloques.push_back(bloque); //cerramos bloque anterior
-            bloque = "";               //reiniciamos bloque actual
-            ident = contar_ident(line);//nueva identacion
-        }else if(ident<contar_ident(line)){
-            ident = contar_ident(line);
-        }
+            i++;//nuevo bloque
+            while(!nodo_if.empty()){
+                auto [k,ident_,e_else] = nodo_if.top();
+                if(ident_<contar_ident(line)||e_else) break;
+                nodos[k].push_back(i);
+                nodo_if.pop();
+            }
+            nodo.push_back(i);
+            nodos.push_back(nodo);
+            nodo.clear();
+
+            Bloques.push_back(bloque);
+            bloque = "";
+            }
+
+        ident = contar_ident(line);
+
         if(es_if(line)){
-            bloque+=line;
+            nodo.push_back(i+1);
+            nodos.push_back(nodo);
+            nodo_if.push({i,contar_ident(line),exis_else(nombre,j,contar_ident(line))});
+            nodo.clear();
+            i++;
+
+            bloque+=cortar_ident(line)+"\n";
+            Bloques.push_back(bloque);
+            bloque = "";
 
         }else if(es_while(line)){
-            cout<<" --> while"<<endl;
+            nodo.push_back(i+1);
+            nodos.push_back(nodo);
+            nodo.clear();
+            i++;
+            nodo.push_back(i+1);
+            nodos.push_back(nodo);
+            nodo.clear();
+            i++;
+
+            Bloques.push_back(bloque);
+            Bloques.push_back(cortar_ident(line+"\n"));
+            bloque = "";
+
         }else if(es_else(line)){
-            cout<<endl;
+            auto [a,b,c] = nodo_if.top();
+            if(b==contar_ident(line) and c){
+                nodos[a].push_back(i);
+                nodo_if.pop();
+            }
         }else if(es_def(line)||es_func(line)){
-            cout<<" --> bloque"<<endl;
+            bloque+=cortar_ident(line)+"\n";
         }
+    j++;
+    }
+
+    Bloques.push_back(bloque);i++;
+    for(int i=0;i<Bloques.size();i++){
+        cout<<"bloque "<<i<<":"<<endl;
+        cout<<Bloques[i]<<endl;
+    }
+    cout<<endl;
+    for(int i=0;i<nodos.size();i++){
+        cout<<i<<": ";
+        for(int x: nodos[i]){
+            cout<<x<<" ";
+        }
+        cout<<endl;
     }
 
     inputFile.close();//inputFile.open() si se quiere reabrir
